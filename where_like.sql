c290937e1e5309ad7fab7b2bbe719ae68d59e116
brunovn2000 <brunovn2009@hotmail.com>
2017-07-10 23:42:43 -0300

    Add files via upload

---
+++ where_like.sql
@@ -0,0 +1,11 @@
+select nome_estado
+from ESTADO
+where nome_estado like '%s%' ;
+
+select *
+from pessoa
+where contatos like '%27%';
+
+select *
+from evento_credito
+where nome_evento like '%l%' ;
