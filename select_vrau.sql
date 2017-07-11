92769f980af7faddb0e7f75e490dbb5a022fd714
brunovn2000 <brunovn2009@hotmail.com>
2017-07-11 00:11:01 -0300

    Add files via upload

---
+++ select_vrau.sql
@@ -0,0 +1,13 @@
+use vrau_eventos ;
+
+select * from estado ;
+select * from cidade ;
+select * from bairro ;
+select * from tipo_local ;
+select * from tipo_evento;
+select * from evento_desconto;
+select * from local ;
+select * from evento_credito ;
+select * from pessoa ;
+select * from perfil ;
+select * from debita ;
